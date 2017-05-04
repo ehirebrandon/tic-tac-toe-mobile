a77cad76a303825e223e298a9a3b4267eb23f99f
Brandon Lai <brandon@brandons-mbp>
2017-05-04 17:43:45 -0400

    Added a Notes.swift file to brainstorm how to model the project. Also, organized the folder by categories i.e. Models, Controllers, Storyboards etc.

---
+++ NeverToes/NeverToes/Notes.swift
@@ -0,0 +1,14 @@
+//
+//  Notes.swift
+//  NeverToes
+//
+//  Created by Brandon Lai on 5/4/17.
+//  Copyright © 2017 Brandon Lai. All rights reserved.
+//
+
+// Views: 1
+// Controllers: 1
+// Models: Players() & AI()
+// --Protocols: Actions for players and AI to share
+
+
---
+++ NeverToes/NeverToes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeverToes
+//
+//  Created by Brandon Lai on 5/4/17.
+//  Copyright © 2017 Brandon Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
