a9ed7d76007898fa72047ee637a137918b7abe69
Brandon Lai <brandon@brandons-mbp.attlocal.net>
2017-05-04 13:23:38 -0400

    added readme.swift for project criteria, problems, ambiguity, assumptions, method of solving, and post project analysis

---
+++ readme.swift
@@ -0,0 +1,54 @@
+// Project Tic Tac Toe GOAT - Requirements set by CMG: (Requirements Paste Verbatim)
+
+//Story
+//
+//As a CMG employee, I want to see how you code a game of Tic Tac Toe, so that I can get a feel for a candidate's skills and strengths.
+//
+//Acceptance criteria
+//
+//1)Application is a stand alone native mobile (java, swift, objective-c) game of tic tac toe
+//2)Computer (AI) will never lose a game
+//3)Application should be able to run without modifications on IOS or Android
+//4)Game will let player choose to be either X or O, computer will take other choice
+//5)Game will let player go first
+//
+//Submission Tips
+//
+//Quality counts! A good submission that takes a while is better than a poor submission quickly.
+//Make sure your submission accurately reflects your development style.
+//Commit early and often, with good messages.
+//Comments and Unit tests are appreciated but not required, if you know good practice, then show us.
+//Research the AI, there are multiple well known algorithms available, show us your implementation.
+//Plagiarism will not be tolerated.
+//Submissions
+//
+//Fork this repo and send us a pull request.
+//If you prefer you can send us a ZIP of your submission, due to email filters, rename the file to *.txt and mention it is a ZIP in your email.
+
+//_______________________Problems/Ambiguity/Assumptions/Methods of solving____________________________________
+//Problems & Ambiguity:
+//1. Application is native: clear: Swift
+//2. AI:
+//      --clear: AI for this simple game means it will be able to start a move and decide a move once the player makes one.
+//      --unclear: In submission, It says that there are multiple algorithms available; not quite sure if this involves algorithms for making next moves, mapping decisions, making optimal moves, or self learning.
+//3. Modifications on iOS: None
+//4. Players will be able to make a choice on choosing X or O 
+//5. Game will let player go first:
+//      --clear: Game is able to make a decision at any point in the game
+//      --unclear: Will the player go first everytime?
+
+//Assumptions:
+//1, 3, 4 are clear without Assumptions
+//2. --AI will not be self learning.
+//   --AI will use my self made algorithm
+//   --AI will not use any well known algorithm for sake of Plagiarism risks or over complication of the simple problem that is needed to be solve.
+//   --AI will make optimal decision by deriving from the algorithm that is base off of my analysis of tic-tac-toe game never-losing strategy
+//5. Game will always let the player go first instead of having AI make any initial moves. 
+
+//Proposal:
+// I will create Tic Tac Toe game natively mobile application in Swift that uses self made algorithm that does not uses any self-learning technique which always let the users go first and choose X or O for user alias with no modifications in iOS. 
+
+
+
+
+//______________________Post Project Completion Analysis______________________________________________________
